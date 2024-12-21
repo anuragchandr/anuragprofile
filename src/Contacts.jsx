fc3664724d3170f0c401dc8322ac51ddd1b588a7
Anurag <nrgchandra@gmail.com>
2024-12-21 19:59:22 +0530

    Enhance portfolio layout and styling with new background and improved components

--- src/Contacts.jsx
+++ src/Contacts.jsx
@@ -1,18 +1,20 @@
 function Contacts() {
   return (
     <div>
+      <hr />
       <a href="https://github.com/anuragchandr" id="id3">
-            <img src="https://t4.ftcdn.net/jpg/00/53/45/31/360_F_53453175_hVgYVz0WmvOXPd9CNzaUcwcibiGao3CL.jpg"/>
+            <img src="https://t4.ftcdn.net/jpg/00/53/45/31/360_F_53453175_hVgYVz0WmvOXPd9CNzaUcwcibiGao3CL.jpg" className="contact"/>
         </a>
         <a href="https://t.me/electrocoders" id="id3">
-            <img src="https://t4.ftcdn.net/jpg/00/53/45/31/360_F_53453175_hVgYVz0WmvOXPd9CNzaUcwcibiGao3CL.jpg"/>
+            <img src="https://t4.ftcdn.net/jpg/00/53/45/31/360_F_53453175_hVgYVz0WmvOXPd9CNzaUcwcibiGao3CL.jpg" className="contact"/>
         </a>
         <a href="https://www.instagram.com/anuragchandra.official?igsh=dTJwZDVpcDliYTlq" id="id3">
-            <img src="https://t4.ftcdn.net/jpg/00/53/45/31/360_F_53453175_hVgYVz0WmvOXPd9CNzaUcwcibiGao3CL.jpg"/>
+            <img src="https://t4.ftcdn.net/jpg/00/53/45/31/360_F_53453175_hVgYVz0WmvOXPd9CNzaUcwcibiGao3CL.jpg" className="contact"/>
         </a>
         <a href="https://youtube.com/@shorts_circuit?si=7nDSEnz7f1JQKwfk" id="id3">
-            <img src="https://t4.ftcdn.net/jpg/00/53/45/31/360_F_53453175_hVgYVz0WmvOXPd9CNzaUcwcibiGao3CL.jpg"/>
+            <img src="https://t4.ftcdn.net/jpg/00/53/45/31/360_F_53453175_hVgYVz0WmvOXPd9CNzaUcwcibiGao3CL.jpg" className="contact"/>
         </a>
+        <hr />
     </div>
   );
 }	
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,6 @@
 function Header() {
     return (
       <header> {/* Change this to a standard HTML element like <header> */}
-        <h1>My Portfolio</h1>
         <a href="#home">Home</a>
         <a href="#about">About</a>
         <a href="#projects">Projects</a>
